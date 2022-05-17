5a46df59c435bd3cdc8c2770cc2b38a09a1d1bcc
snorkungen <teuvo2004@gamil.com>
2022-05-17 21:22:15 +0300

    update createElement.ts

--- src/lib/createElement.ts
+++ src/lib/createElement.ts
@@ -27,16 +27,12 @@ export const createElementNS = (
     elemName: string,
     ...attributes: string[]
 ) => {
-    const element = document.createElementNS("http://www.w3.org/1999/xhtml", elemName);
+    const element = document.createElementNS("http://www.w3.org/2000/svg", elemName);
 
     for (const attribute of attributes) {
         const [qualifiedName, value] = attribute.split("=");
         if (!qualifiedName) continue;
-        if (qualifiedName == "content") {
-            element.textContent = value;
-        } else {
-            element.setAttributeNS("http://www.w3.org/1999/xhtml", qualifiedName, value);
-        }
+        element.setAttribute( qualifiedName, value);
     }
 
     if (parent) {
