853615aa191da932bc63abb2d6399691fc886722
snorkungen <teuvo2004@gamil.com>
2022-05-19 15:17:30 +0300

    update createElement.ts

--- src/lib/createElement.ts
+++ src/lib/createElement.ts
@@ -1,11 +1,13 @@
-export const createElement = (
-    parent: null | HTMLElement,
-    elemName: string,
-    ...attributes: string[]
-) => {
-    const element = document.createElement(elemName);
-
+const attributesHandler = <K extends keyof (HTMLElementEventMap & SVGElementEventMap)>(element: HTMLElement | SVGElement, ...attributes: (string | {
+    type: K;
+    listener: (ev: Event | HTMLElementEventMap[K] | SVGElementEventMap[K]) => any;
+    options?: (boolean | AddEventListenerOptions) | (boolean | AddEventListenerOptions)
+})[]) => {
     for (const attribute of attributes) {
+        if (typeof attribute === "object") {
+            element.addEventListener(attribute.type, attribute.listener, attribute.options)
+            continue;
+        }
         const [qualifiedName, value] = attribute.split("=");
         if (!qualifiedName) continue;
         if (qualifiedName == "content") {
@@ -14,6 +16,17 @@ export const createElement = (
             element.setAttribute(qualifiedName, value);
         }
     }
+
+}
+
+export const createElement = (
+    parent: null | HTMLElement,
+    elemName: string,
+    ...attributes: Parameters<typeof attributesHandler>[1][]
+) => {
+    const element = document.createElement(elemName);
+
+    attributesHandler(element, ...attributes);
 
     if (parent) {
         parent.appendChild(element);
@@ -25,19 +38,16 @@ export const createElement = (
 export const createElementNS = (
     parent: null | HTMLElement | SVGElement,
     elemName: string,
-    ...attributes: string[]
+    ...attributes: Parameters<typeof attributesHandler>[1][]
 ) => {
     const element = document.createElementNS("http://www.w3.org/2000/svg", elemName);
 
-    for (const attribute of attributes) {
-        const [qualifiedName, value] = attribute.split("=");
-        if (!qualifiedName) continue;
-        element.setAttribute( qualifiedName, value);
-    }
-
+    attributesHandler(element, ...attributes);
+ 
     if (parent) {
         parent.appendChild(element);
     };
 
     return element;
 };
+EventTarget
