33317ca6b9e79a60a863f82b1b83075ee034541d
snorkungen <teuvo2004@gamil.com>
2022-05-23 14:35:03 +0300

    update orgChart.ts

--- src/orgChart.ts
+++ src/orgChart.ts
@@ -1,7 +1,7 @@
 import { createElementNS } from "./lib";
 import { app } from "./main";
 
-let width = 300, height = 200;
+let width = 600, height = 200;
 let personWidth = 60, personHeight = 20;
 
 type Person = {
@@ -13,12 +13,15 @@ const org: Person = {
     name: "Boss",
     sub: [
         { name: "Assistant", sub: [{ name: "Assistant-Assistant" }, { name: "Noob" }] },
-        { name: "Master-Assasin", sub: [{ name: "Assasin", sub: [{ name: "Assasin-Apprentice" }, { name: "Noob" }] }] },
+        { name: "Secret Helper",sub:[{name : "Liam"}] },
+        { name: "Secret Helper" },
+        { name: "Master-Assasin", sub: [{ name: "Assasin", sub: [{ name: "Assasin-Apprentice", sub: [{ name: "Joe" }, { name: "Michelle" }] }, { name: "Noob" }] }] },
+        { name: "Master-Assasin", sub: [{ name: "Assasin", sub: [{ name: "Assasin-Apprentice", sub: [{ name: "Joe" }, { name: "Michelle" }] }, { name: "Noob" }] }] },
     ]
 }
 
 export const orgChart = () => {
-    const SVGElement = createElementNS(app, "svg", `viewBox=0 0 ${width} ${height}`, `width=${width + height}`, {
+    const SVGElement = createElementNS(app, "svg", `viewBox=0 0 ${width} ${height}`, `width=${width + height}`,"overflow=scroll", {
         children: [
             createElementNS(null, "rect", `width=${width}`, `height=${height}`, `fill=${"red"}`)
         ]
@@ -42,28 +45,48 @@ export const orgChart = () => {
             }
         }) as SVGRectElement;
 
+        createElementNS(groupElement, "text", `content=${person.name}`, {
+            attributes: {
+                x: startX,
+                y: startY,
+                dy: personHeight / 2,
+                "font-size": "11px",
+                textLength: personWidth,
+                lengthAdjust: "spacingAndGlyphs",
+                fill: "white"
+            }
+        })
+
         let sub: SVGRectElement[] = [];
 
         if (person.sub) {
+            let sub_sub_count = 0;
+            for (const subPerson of person.sub) {
+                if (subPerson.sub && subPerson.sub.length) sub_sub_count++;
+            }
             for (const subPerson of person.sub) {
                 let i = person.sub.indexOf(subPerson);
                 let w = availWidth / person.sub.length, offset = w * i + availWidthStart;
                 let x = calcXCenter(w) + offset, y = startY + (personHeight * 2);
 
+                let subI = i ? i - (person.sub.length - sub_sub_count) : i, subAvailWidth = availWidth / sub_sub_count;
+
                 sub.push(
-                    drawPerson(subPerson, x, y, w, w * i + availWidthStart)
+                    drawPerson(subPerson, person.sub.length == 1 ? startX : x, y, subAvailWidth, subAvailWidth * subI + availWidthStart)
                 )
             }
         }
 
         for (const subRect of sub) {
-            createElementNS(groupElement, "line", {
+            createElementNS(groupElement, "path", {
                 attributes: {
-                    x1: startX + personWidth / 2,
-                    y1: startY + personHeight,
-                    x2: subRect.x.animVal.value + personWidth / 2,
-                    y2: subRect.y.animVal.value,
-                    stroke: "blue"
+                    fill: "none",
+                    stroke: "blue",
+                    d: `M${startX + personWidth / 2},${startY + personHeight}
+                    V${startY + personHeight * 1.5}
+                    H${subRect.x.animVal.value + personWidth / 2}
+                    V${subRect.y.animVal.value}
+                    `
                 }
             })
         }
